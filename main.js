c72781c72414393c1023aa3a8b8a9a31b89c4c1f
Anna527316 <jhon.w.smith.2024@gmail.com>
2025-09-20 20:26:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
 
 const token = '8025245630:AAFROgo4E5lbAnPgrkGsXXEjfkcbtFDjp_U';
 const bot = new TelegramBot(token, { polling: true });
 
-const PORT = process.env.PORT || 10000;
+const PORT = 10000;
 require('http')
   .createServer((_, res) => res.end('OK'))
   .listen(PORT, () => console.log(`Dummy health-check server on :${PORT}`));

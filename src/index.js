594747d03005729ddaaf3b7dd9db40de2b0ba98f
Anna527316 <jhon.w.smith.2024@gmail.com>
2025-09-23 13:30:16 -0400

    Added a new comman: /wave - Get a hello from the bot

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { ALLOWED_USERS } from './config/defaults.js';
 import TelegramBot from 'node-telegram-bot-api';
 import { createServer } from 'http';
 
-
 const token = process.env.TOKEN
 const bot = new TelegramBot(token, { polling: true });
 
@@ -12,21 +11,37 @@ const PORT = 10000;
 createServer((_, res) => res.end('OK'))
   .listen(PORT, () => console.log(`Dummy health-check server on :${PORT}`));
 
+// BOT BEHAVIOUR
+
+// Message event
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const senderId = msg.from.id;
+  const senderName = msg.from.first_name;
   const messageText = msg.text;
-
-  console.log(senderId);
   
   if (ALLOWED_USERS.includes(senderId)) {
+
+    // Commands
     switch (messageText) {
+
       case '/start':
-        bot.sendMessage(chatId, 'Welcome!');
+        bot.sendMessage(chatId, `Welcome, ${senderName}!`);
         break;
+
+      case '/help':
+        const helpText = "This is a bot for testing purposes. \n\nCommands:\n/start : Start the bot\n/help: Get a list of the bot's commands and how to use them\n/wave : Get a 'Hello!' from the bot"
+        bot.sendMessage(chatId, helpText);
+        break;
+
+      case '/wave':
+        bot.sendMessage(chatId, 'Hello!');
+        break;
+      
       default:
         bot.sendMessage(chatId, 'That command is not recognized.');
         break;
+
     }
   } else {
     bot.sendMessage(chatId, 'This user is not authorized to send messages to this bot!');
